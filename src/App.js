37c0de6314e5b87909adc79a8178d1787100b390
Flayelle Camille <camille@deCamille.local>
2022-02-23 10:46:38 +0100

    ajout d'éléments

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
+import { useState } from 'react'
 
 function App() {
-  const items = ["Pain", "Steak", "Tomate", "Sauce burger", "Frites"]
+  const [items,setItems] = useState(["Pain", "Steak", "Tomate", "Sauce burger", "Frites"])
+  const [addItem, setAddItem] = useState("")
+  
   const lists = 
     items.map((item, index) => (
       <div key={index}>
@@ -9,13 +12,31 @@ function App() {
         <label htmlFor={index}>{item}</label>
       </div>
     ))
+  
+   const handleChange = (e) => {
+     setAddItem(e.target.value)
+   }
 
+   const handleSubmit = (e) => {
+      setItems(
+        [...items,
+          addItem]
+      )
+     e.preventDefault()
+   }
+  
   return (
     <div class="list">
       <h1>Liste</h1>
-      <form>
-         {lists}
-         
+       <form onSubmit={handleSubmit}>
+        {lists}
+        <div>
+           <p>Ajouter un élément à la liste (🍕 ou 🍪) : </p>
+           <textarea onChange={handleChange} id="addItem" name="addItem" value={addItem} onChange={handleChange}/>
+          </div>
+         <div>
+           <button type="submit">Ajouter</button>
+         </div>
        </form>
     </div>
   )
