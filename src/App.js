20007d6746633ecf7d4f9b0d6ca8ca39f42cec9d
Flayelle Camille <camille@deCamille.local>
2022-02-23 10:21:12 +0100

    les checks-lits

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import './App.css';
 
 function App() {
   const items = ["Pain", "Steak", "Tomate", "Sauce burger", "Frites"]
-  const lists = items.map((item,index) => (
-    <div key={index}>
-      {item}
-    </div>
-  ))
-  return (
-    <div>
-      <h1>Liste</h1>
-      <div>
-        {lists}
+  const lists = 
+    items.map((item, index) => (
+      <div key={index}>
+        <input type="checkbox" id={index} name={index}/>
+        <label htmlFor={index}>{item}</label>
       </div>
+    ))
+
+  return (
+    <div class="list">
+      <h1>Liste</h1>
+      <form>
+         {lists}
+         
+       </form>
     </div>
   )
 }
